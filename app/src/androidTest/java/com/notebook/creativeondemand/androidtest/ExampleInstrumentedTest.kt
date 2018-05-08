392840980ed2a1f1f4834f85c80fdb4054748664
Jimmy Pino <jimmypino@stapp.com>
2018-05-08 09:35:27 -0400

    Initial commit

---
+++ app/src/androidTest/java/com/notebook/creativeondemand/androidtest/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.notebook.creativeondemand.androidtest
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.notebook.creativeondemand.androidtest", appContext.packageName)
+    }
+}
